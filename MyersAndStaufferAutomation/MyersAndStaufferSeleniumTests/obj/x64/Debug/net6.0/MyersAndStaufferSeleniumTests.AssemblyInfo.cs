6c38201a426811c167d4e8e63e8f8353c462bb1c
Mobin <mohammad.mobin@mechlintech.com>
2023-12-29 23:51:01 +0530

    Login Changes

--- MyersAndStaufferAutomation/MyersAndStaufferSeleniumTests/obj/x64/Debug/net6.0/MyersAndStaufferSeleniumTests.AssemblyInfo.cs
+++ MyersAndStaufferAutomation/MyersAndStaufferSeleniumTests/obj/x64/Debug/net6.0/MyersAndStaufferSeleniumTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyersAndStaufferSeleniumTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c646189c34f973f625393d0815b7f79cee32914")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyersAndStaufferSeleniumTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyersAndStaufferSeleniumTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
