6d4320625274c706b1c78efc4fa5af113a57fbba
Mobin <mohammad.mobin@mechlintech.com>
2024-01-18 17:34:31 +0530

    Changes for Latest API

---
+++ SeleniumReportAPI/Models/Environments.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SeleniumReportAPI.Models
+{
+    public class Environments
+    {
+        [Key]
+        public int EnvironmentId { get; set; }
+        [Required(ErrorMessage = "Environment Name is required."), Column("EnvironmentName", TypeName = "varchar(100)")]
+        public string EnvironmentName { get; set; }
+    }
+}
