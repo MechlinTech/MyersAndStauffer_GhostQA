9ccacaf6cf669c900454a84896c2523137ae73b2
Nitin Srivastava <Nitin.Srivastava@mechlintech.com>
2024-01-20 03:13:34 +0530

    fixes

--- MyersAndStaufferAutomation/MyersAndStaufferSeleniumTests/obj/x64/Debug/net6.0/MyersAndStaufferSeleniumTests.AssemblyInfo.cs
+++ MyersAndStaufferAutomation/MyersAndStaufferSeleniumTests/obj/x64/Debug/net6.0/MyersAndStaufferSeleniumTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyersAndStaufferSeleniumTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a11089455b17897edfb8fe0f1f605afeb9aba4eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+471fa8e34311008396c60a7131c024e5ab3bee94")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyersAndStaufferSeleniumTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyersAndStaufferSeleniumTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
