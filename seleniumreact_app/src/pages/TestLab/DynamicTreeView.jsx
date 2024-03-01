559fe5005c73990c4648b1fcd473206df828dca3
Manoj1804 <manojsinghrawat1804@gmail.com>
2024-03-01 19:49:10 +0530

    test lab treeview fix bug of tree

--- seleniumreact_app/src/pages/TestLab/DynamicTreeView.jsx
+++ seleniumreact_app/src/pages/TestLab/DynamicTreeView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useCallback } from "react";
 import { useStylesTree } from "./styleTree";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
@@ -36,61 +36,15 @@ const data = [
   }
 ];
 
-const Card = ({ handleEdit, editMode, editData, setEditData, setEditMode, handleEditChange, handleKeyPressEdit, editModeValue, setListData, data, expandedData = true, nodeData = 0, handleCRUDAtParent, nodeCount = 0, handleNodeCount, expandedInputId, setExpandedInputId, handleTask }) => {
+const Card = ({newElementName, setNewElementName,
+  toggleExpand,
+  handleCRUDCancel ,
+  handleKeyPress,
+  handleDelete,
+  setExpanded,expanded,
+  handleCRUD, handleEdit, editMode, editData, setEditData, setEditMode, handleEditChange, handleKeyPressEdit,  setListData, data, expandedData = true, nodeData = 0, handleCRUDAtParent, nodeCount = 0, handleNodeCount, expandedInputId, setExpandedInputId, handleTask }) => {
   const styleClass = useStylesTree();
-  const [expanded, setExpanded] = useState(false);
-  const [newElementName, setNewElementName] = useState(''); // State to store the value of the input field
-  const toggleExpand = () => {
-    setExpanded(!expanded);
-  };
-  const handleCRUDCancel = () => {
-    setNewElementName('');
-    setExpandedInputId(0);
-  };
-  const handleCRUD = (parentId) => {
-    // For demonstration purposes, let's add a new element if the node count is less than 4
-    if (nodeCount < 4) {
-      setExpandedInputId(parentId);
-    } else {
-      alert('Maximum node limit reached.');
-    }
-  };
-  const handleCRUDNewItem = (parentId) => {
-    // For demonstration purposes, let's add a new element if the node count is less than 4
-    if (nodeCount < 4) {
-      setExpandedInputId(null); // Hide the input field
-      if (newElementName) {
-        const newId = Math.max(...data.map(item => item.id)) + 1;
-        const newItem = { name: newElementName, id: -newId, parentId: parentId };
-        handleCRUDAtParent(newItem);
-        setExpanded(true);
-        setNewElementName('');
-      }
-    } else {
-      alert('Maximum node limit reached.');
-    }
-  };
-
-  const handleKeyPress = (event, parentId) => {
-    if (event.key === 'Enter') {
-      handleCRUDNewItem(parentId);
-    }
-  };
-  const handleDelete = (itemId) => {
-    console.log(itemId, data);
-    const itemToDelete = data.find(item => item.id === itemId);
-    const childrenToDelete = data.filter(item => item.parentId === itemId);
-    const updatedData = data.filter(item => item.id !== itemId && item.parentId !== itemId);
-    const parentIdOfParent = itemToDelete ? itemToDelete.parentId : null;
-    const updatedChildren = childrenToDelete.map(child => ({
-      ...child,
-      parentId: parentIdOfParent,
-    }));
-    const newData = [...updatedData, ...updatedChildren];
-    console.log(newData, itemId, itemToDelete, childrenToDelete);
-    setListData(newData);
-  };
-
+  console.log(data)
   return (
     <>
       <ul style={{ display: (expandedData ? 'block' : 'none') }} className={(nodeData == 0 ? styleClass.rootNodeFolder : styleClass.child)}>
@@ -108,7 +62,7 @@ const Card = ({ handleEdit, editMode, editData, setEditData, setEditMode, handle
                     <div className={styleClass.updateEdit}>
                       <input type="text" value={editData} className={styleClass.editTheFolder}
                         onChange={(e) => handleEditChange(item.id, e.currentTarget.value)}
-                        onKeyPress={(event) => handleKeyPressEdit(event, item.id)}
+                        onKeyPress={(event) => handleKeyPressEdit(event, item.id,nodeCount)}
                       />
                       <CancelIcon sx={{ color: '#f74d4d' }} onClick={() => handleEdit(item.id, item.name, 'cancel')} />
                     </div>
@@ -128,22 +82,26 @@ const Card = ({ handleEdit, editMode, editData, setEditData, setEditMode, handle
                   }}>
                     <EditIcon sx={{ color: '#654df7' }} onClick={() => handleEdit(item.id, item.name)} style={{ cursor: 'pointer', marginLeft: '10px' }} />
                     <DeleteIcon sx={{ color: '#f74d4d' }} onClick={() => handleDelete(item.id)} style={{ cursor: 'pointer' }} />
-                    {nodeCount < 4 && (
-                      <AddIcon sx={{ color: '#654df7' }} onClick={() => handleCRUD(item.id)} style={{
+                    {nodeCount < 5 && (
+                      <AddIcon sx={{ color: '#654df7' }} onClick={(event) => handleCRUD(event,item.id)} style={{
                         marginLeft: 'auto'
                       }} />
                     )}
                   </div>
 
                 </div>
-                <div className={styleClass.updateEdit} style={{ display: (expandedInputId === item.id ? 'block' : 'none') }}>
-                  <input type="text"
-                    className={styleClass.editTheFolder}
-                    value={newElementName}
-                    onChange={(e) => setNewElementName(e.target.value)}
-                    onKeyPress={(event) => handleKeyPress(event, item.id)} />
-                  <CancelIcon sx={{ color: '#f74d4d' }} onClick={() => handleCRUDCancel()} />
-                </div>
+                {(expandedInputId === item.id && expandedInputId != null ) && 
+                <div className={styleClass.updateEdit} style={{ display: (expandedInputId === item.id ? 'block' : 'block') }}>
+                <input type="text"
+                  className={styleClass.editTheFolder}
+                  value={newElementName}
+                  key={item.id}
+                  onChange={(e) => setNewElementName(e.target.value)}
+                  onKeyPress={(event) => handleKeyPress(event, item.id,nodeCount)} />
+                <CancelIcon sx={{ color: '#f74d4d' }} onClick={() => handleCRUDCancel()} />
+              </div>
+                }
+                
                 {data.some(child => child.parentId === item.id) && (
                   <Card
                     data={data}
@@ -153,7 +111,7 @@ const Card = ({ handleEdit, editMode, editData, setEditData, setEditMode, handle
                     handleCRUDAtParent={handleCRUDAtParent}
                     expandedData={expanded}
                     getparentId={item.parentId}
-                    nodeCount={nodeCount + 1}
+                    nodeCount={nodeData + 1}
                     handleNodeCount={handleNodeCount}
                     expandedInputId={expandedInputId}
                     setExpandedInputId={setExpandedInputId}
@@ -165,6 +123,15 @@ const Card = ({ handleEdit, editMode, editData, setEditData, setEditMode, handle
                     setEditData={setEditData}
                     editMode={editMode}
                     setEditMode={setEditMode}
+                    newElementName={newElementName} 
+                    setNewElementName={setNewElementName}                   
+                    handleCRUD={handleCRUD}
+                    expanded={expanded} 
+                    setExpanded={setExpanded}
+                    toggleExpand={toggleExpand}
+                    handleCRUDCancel ={handleCRUDCancel}
+                    handleKeyPress={handleKeyPress}
+                    handleDelete={handleDelete} 
                   />
                 )}
               </li>
@@ -183,19 +150,54 @@ const DynamicTreeView = ({ TestCaseHandle }) => {
   const [expandedInputId, setExpandedInputId] = useState(null);
   const [editData, setEditData] = useState(''); // State to store the value of the input field
   const [editMode, setEditMode] = useState(0); // State to store the value of the input field
+  const [expanded, setExpanded] = useState(false);
 
+  const [newElementName, setNewElementName] = useState(''); // State to store the value of the input field
+  const handleCRUD = (event,parentId) => {
+    event.preventDefault();
+    console.log(parentId);
+    // For demonstration purposes, let's add a new element if the node count is less than 4
+    if (nodeCount < 5) {
+      setExpandedInputId(parentId);
+    } else {
+      alert('Maximum node limit reached.');
+    }
+  };
   const handleCRUDAtParent = (newItem) => {
     setListData([...listData, newItem]);
   };
 
+  const handleCRUDNewItem = useCallback((parentId,nodeData) => {
+    
+   
+    if (nodeCount < 5) {
+      setExpandedInputId(null); // Hide the input field
+      if (newElementName) {
+        const newId = Math.max(...listData.map(item => item.id)) + 1;
+        const newItem = { name: newElementName, id: newId, parentId: parentId,new:'new' };
+        handleCRUDAtParent(newItem);
+        setExpanded(true);
+        setNewElementName('');
+      }
+    } else {
+      alert('Maximum node limit reached.');
+    }
+    console.log(listData);
+  }, [nodeCount, newElementName, listData, handleCRUDAtParent]);
+  
+
+
   const handleNodeCount = (count) => {
+    
     setNodeCount(count);
   };
-  const handleKeyPressEdit = (event, itemId) => {
+  const handleKeyPressEdit = (event, itemId,node) => {
+
+  
     if (event.key === 'Enter') {
       setEditMode(0);
-      const itemToEdit = data.find(item => item.id === itemId);
-      const newData = data.filter((item) => {
+      const itemToEdit = listData.find(item => item.id === itemId);
+      const newData = listData.filter((item) => {
         if (item.id !== itemId) {
           return item;
         } else if (item.id === itemId) {
@@ -203,13 +205,14 @@ const DynamicTreeView = ({ TestCaseHandle }) => {
           return item;
         }
       });
+      console.log()
       setListData(newData);
       setEditData('');
     }
   };
   const handleEdit = (itemId, name, mode = 'edit') => {
     if (mode == 'edit') {
-      setEditMode(current => itemId);
+      setEditMode( itemId);
       setEditData(name);
     } else if (mode == 'cancel') {
       setEditData('');
@@ -220,6 +223,36 @@ const DynamicTreeView = ({ TestCaseHandle }) => {
 
   const handleEditChange = (itemId, name) => {
     setEditData(name);
+  };
+
+
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
+  const handleCRUDCancel = () => {
+    setNewElementName('');
+    setExpandedInputId(0);
+  };
+
+
+  const handleKeyPress = (event, parentId,nodeData) => {
+    if (event.key === 'Enter') {
+      handleCRUDNewItem(parentId,nodeData);
+    }
+  };
+  const handleDelete = (itemId) => {
+    console.log(itemId, data);
+    const itemToDelete = data.find(item => item.id === itemId);
+    const childrenToDelete = data.filter(item => item.parentId === itemId);
+    const updatedData = data.filter(item => item.id !== itemId && item.parentId !== itemId);
+    const parentIdOfParent = itemToDelete ? itemToDelete.parentId : null;
+    const updatedChildren = childrenToDelete.map(child => ({
+      ...child,
+      parentId: parentIdOfParent,
+    }));
+    const newData = [...updatedData, ...updatedChildren];
+    console.log(newData, itemId, itemToDelete, childrenToDelete);
+    setListData(newData);
   };
 
   return (
@@ -241,7 +274,18 @@ const DynamicTreeView = ({ TestCaseHandle }) => {
       handleNodeCount={handleNodeCount} 
       expandedInputId={expandedInputId} 
       setExpandedInputId={setExpandedInputId} 
-      setListData={setListData} />
+      setListData={setListData} 
+      newElementName={newElementName} 
+      setNewElementName={setNewElementName}
+     
+      handleCRUD={handleCRUD}
+      expanded={expanded} 
+      setExpanded={setExpanded}
+      toggleExpand={toggleExpand}
+      handleCRUDCancel ={handleCRUDCancel}
+      handleKeyPress={handleKeyPress}
+      handleDelete={handleDelete} 
+      />
     </div>
   );
 };
