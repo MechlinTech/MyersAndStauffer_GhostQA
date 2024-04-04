4e19a7cd3945332dccaac2d2e7b4cd173e7c053f
Diljot Singh <diljots99@gmail.com>
2024-04-04 20:29:46 +0530

    migrations

--- SeleniumReportAPI/migrations.sh
+++ SeleniumReportAPI/migrations.sh
@@ -1,3 +1,8 @@
 ﻿#!/bin/bash
 dotnet ef database update
 
+# Execute the SQL script for stored procedures
+echo "Executing AllGhostQA_SP.sql against the database..."
+/opt/mssql-tools/bin/sqlcmd -S db,1433 -U sa -P P@ssw0rd@123### -i /SeleniumReportAPI/SqlScript/AllGhostQA_SP.sql
+
+tail -f /dev/null
