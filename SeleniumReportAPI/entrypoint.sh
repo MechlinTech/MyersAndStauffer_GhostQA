14444b38b32fae4cb43aa99113de560c4a056a06
Diljot Singh <diljots99@gmail.com>
2024-04-04 16:39:35 +0530

    database migration fix

--- SeleniumReportAPI/entrypoint.sh
+++ SeleniumReportAPI/entrypoint.sh
@@ -4,7 +4,7 @@
 wait_for_sql() {
     echo "Checking SQL Server connection..."
 
-    while ! /opt/mssql-tools/bin/sqlcmd -S db,1433 -U sa -P P@ssw0rd@123### -Q "SELECT 1" > /dev/null 2>&1; do
+    while ! /opt/mssql-tools/bin/sqlcmd -S db -U sa -P P@ssw0rd@123### -Q "SELECT 1" > /dev/null 2>&1; do
         echo "Waiting for SQL Server to be available..."
         sleep 1
     done
