71d13d47a14b632c8cc779078a546b978f5752bb
Diljot Singh <79603631+diljots99@users.noreply.github.com>
2024-04-08 21:14:41 +0530

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,5 @@
 # Define the URL of the Docker Compose file
-$composeFileUrl = "https://raw.githubusercontent.com/MechlinTech/MyersAndStauffer_GhostQA/main/deploy.yml"
+$composeFileUrl = "https://raw.githubusercontent.com/Ghost-QA/GhostQA/main/deploy.yml"
 
 # Download the Docker Compose file
 Invoke-WebRequest -Uri $composeFileUrl -OutFile "docker-compose.yml"
