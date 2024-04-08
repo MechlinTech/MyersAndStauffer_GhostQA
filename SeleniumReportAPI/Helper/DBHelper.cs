ffa78e3b6aa7a01f70f24b90fbb20f0badfd8643
Hosting <Hosting@CODEARREST>
2024-04-08 05:29:49 -0400

    changes in appsetting.

--- SeleniumReportAPI/Helper/DBHelper.cs
+++ SeleniumReportAPI/Helper/DBHelper.cs
@@ -994,6 +994,7 @@ namespace SeleniumReportAPI.Helper
             var fromEmail = _configuration["EmailDetails:EmailUsername"];
             var hostName = _configuration["EmailDetails:EmailHost"];
             var subject = "Invitation to Join Our Platform";
+            var invitationUrl = _configuration["InvitationUrl:url"];
             if (Mailtype.Equals("Invitation"))
             {
                 BodyString = @"<!DOCTYPE html>
@@ -1012,7 +1013,7 @@ namespace SeleniumReportAPI.Helper
                                   <p>Dear [" + toEmail.ToUpper() + @"],</p>
                                   <p>We are thrilled to invite you to join GhostQA Plateform! 🌟</p>
                                   <p>To accept your invitation and immerse yourself in the QA adventure, simply click the button below:</p>
-                                  <p><a href=""http://codearrest.dyndns.org:3009/AcceptInvitation/" + toEmail + @""" style=""background-color: #654DF7; border: none; color: white; padding: 15px 25px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 8px;"">View Invitation</a></p>
+                                  <p><a href=""" + invitationUrl + toEmail + @""" style=""background-color: #654DF7; border: none; color: white; padding: 15px 25px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 8px;"">View Invitation</a></p>
                                 </td>
                               </tr>
                             </table>
@@ -1038,7 +1039,7 @@ namespace SeleniumReportAPI.Helper
                                   <p> Thank you for accepting invitation here is your temprory password:</p>
                                   <em><b> Password: </b> Test@123 </em>
                                   <p> If you want to change your password follow below link </p>
-                                  <p><a href=""http://codearrest.dyndns.org:3009/ChangePassword/" + toEmail + @""" style=""background-color: #654DF7; border: none; color: white; padding: 15px 25px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 8px;"">Change Password</a></p>
+                                  <p><a href=""" + invitationUrl + toEmail + @""" style=""background-color: #654DF7; border: none; color: white; padding: 15px 25px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 8px;"">Change Password</a></p>
                                 </td>
                               </tr>
                             </table>
