19e2316684d0212d8a6af36ccf4d07ff7d76d93c
Diljot Singh <diljots99@gmail.com>
2024-04-10 00:56:05 +0530

    At Most 12 records

--- djangoapp/ghostqa/performace_test/utils/process_row_daata.py
+++ djangoapp/ghostqa/performace_test/utils/process_row_daata.py
@@ -10,27 +10,27 @@ def group_and_average(json_data):
     for data_point in sorted_json_data:
         data_point["datetime"] = datetime.fromtimestamp(data_point["timeStamp"] /1000)
     # Find the total time span covered by the data
-    total_time_span = sorted_json_data[-1]["timeStamp"] - sorted_json_data[0]["timeStamp"]
-    # Find the total time span covered by the data
-    # total_time_span = sorted_json_data[-1]["datetime"] - sorted_json_data[0]["datetime"]
+    total_time_span = sorted_json_data[-1]["datetime"] - sorted_json_data[0]["datetime"]
     
 
     
-    # Calculate interval seconds to achieve approximately 10 items per interval
-    # interval_seconds_old = total_time_span.total_seconds()  / (len(sorted_json_data) / 10)
-    interval_seconds = total_time_span*1000 /  10
+    # Calculate interval size to achieve approximately 10 intervals
+    interval_size = total_time_span / 10
+
     # Initialize dictionary to hold grouped data
     grouped_data = defaultdict(list)
 
+    # Initialize the start interval
+    interval_start = sorted_json_data[0]["datetime"]
+
     # Iterate through each data point in the sorted JSON
     for data_point in sorted_json_data:
-        # # Calculate the interval timestamp
-        # interval_timestamp = data_point["timeStamp"] // (interval_seconds * 1000)
         # Calculate the interval datetime
-        interval_datetime = data_point["datetime"].replace(microsecond=0, second=0) + timedelta(milliseconds=interval_seconds)
-
-        # Add data point to the corresponding interval group
-        grouped_data[interval_datetime].append(data_point)
+        while data_point["datetime"] >= interval_start + interval_size:
+            # Add data point to the corresponding interval group
+            grouped_data[interval_start].append(data_point)
+            # Move to the next interval
+            interval_start += interval_size
 
     # Initialize list to hold averaged data
     averaged_data = []
@@ -48,10 +48,11 @@ def group_and_average(json_data):
         total_percentile_90 = 0
         total_hits_per_second = 0
         total_errors_per_second = 0
-
+        total_timestamp = 0
         
         # Iterate through data points in the group
         for data_point in data_points:
+            total_timestamp += data_point.get("timeStamp", 0) if data_point.get("timeStamp") is not None else 0
             total_connect += data_point.get("Connect", 0) if data_point.get("Connect") is not None else 0
             total_latency += data_point.get("Latency", 0) if data_point.get("Latency") is not None else 0
             total_elapsed += data_point.get("elapsed", 0) if data_point.get("elapsed") is not None else 0
@@ -65,6 +66,7 @@ def group_and_average(json_data):
 
         # Calculate average values
         num_data_points = len(data_points)
+        average_timeStamp = total_timestamp / num_data_points if num_data_points > 0 else 0
         average_connect = total_connect / num_data_points if num_data_points > 0 else 0
         average_latency = total_latency / num_data_points if num_data_points > 0 else 0
         average_elapsed = total_elapsed / num_data_points if num_data_points > 0 else 0
@@ -94,8 +96,7 @@ def group_and_average(json_data):
 
         # Append averaged data for the interval
         result = data_points[0].copy()
-        # result['interval_timestamp'] = interval * interval_seconds * 1000
-        result['interval_datetime'] = interval_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        result['timeStamp'] = average_timeStamp
         result['Connect'] = average_connect
         result['Latency'] = average_latency
         result['elapsed'] = average_elapsed
@@ -107,6 +108,7 @@ def group_and_average(json_data):
         result['Hits_Per_Second'] = average_hits_per_second
         result['Errors_Per_Second'] = average_errors_per_second
         averaged_data.append(result)
-
+    averaged_data.append(sorted_json_data[-1].copy())
+    averaged_data.insert(0,sorted_json_data[0].copy())
     return averaged_data
 
