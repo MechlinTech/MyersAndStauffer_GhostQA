fe8b456d7bc65aa5178ad8af7bdeda85d94a1737
Arpit4121 <arpits4121@gmail.com>
2024-05-06 20:28:11 +0530

    worked on restpassword screen

--- seleniumreact_app/src/Routes/AuthNav.jsx
+++ seleniumreact_app/src/Routes/AuthNav.jsx
@@ -1,17 +1,19 @@
-import React, { lazy } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Invitation from '../pages/Invitaion'
-import ChangePassword from '../pages/Account/ChangePassword'
-import ForgotPassword from '../pages/ForgotPassword'
-const Auth = lazy(() => import("../pages/Auth"))
+import React, { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import Invitation from "../pages/Invitaion";
+import ChangePassword from "../pages/Account/ChangePassword";
+import ForgotPassword from "../pages/ForgotPassword";
+import ResetPassword from "../pages/ResetPassword";
+const Auth = lazy(() => import("../pages/Auth"));
 
 export default function AuthNav() {
   return (
     <Routes>
-        <Route path='*' element={<Auth/>}/>
-        <Route path="/AcceptInvitation/:toEmail" element={<Invitation />} />
-        <Route path="/ChangePassword/:emailId" element={<ChangePassword />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="*" element={<Auth />} />
+      <Route path="/AcceptInvitation/:toEmail" element={<Invitation />} />
+      <Route path="/ChangePassword/:emailId" element={<ChangePassword />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
     </Routes>
-  )
+  );
 }
