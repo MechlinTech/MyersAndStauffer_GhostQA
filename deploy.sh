2ff77fb4c30e7d068700b83ed0998dc0c5209350
soniakashyap2002 <163018749+soniakashyap2002@users.noreply.github.com>
2024-05-30 20:54:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Define the URL of the Docker Compose file
-composeFileUrl="https://raw.githubusercontent.com/MechlinTech/MyersAndStauffer_GhostQA/5065_sonia/docker-compose.yml"
+composeFileUrl="https://raw.githubusercontent.com/Ghost-QA/GhostQA/main/deploy.yml"
 
 # Download the Docker Compose file
 wget "$composeFileUrl" -O docker-compose.yml
 
+docker pull ghostqa/codeengine:latest
+docker pull mcr.microsoft.com/mssql/server:2019-latest
+docker pull ghostqa/dotnetapi-migrations:latest
+docker pull ghostqa/dotnetapi:latest
+docker pull ghostqa/webui:latest
 
 # Run docker-compose up command
 docker compose up -d
