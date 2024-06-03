b531d57b45156954ffd8ad6b3149d95bd733363d
Diljot Singh <diljots99@gmail.com>
2024-06-03 14:05:18 -0700

    update _basepath

--- MyersAndStaufferAutomation/MyersAndStaufferSeleniumTests/Arum/Mississippi/TestFile/TestExecutor.cs
+++ MyersAndStaufferAutomation/MyersAndStaufferSeleniumTests/Arum/Mississippi/TestFile/TestExecutor.cs
@@ -7,7 +7,7 @@ namespace MyersAndStaufferSeleniumTests.Arum.Mississippi.TestFile
         private static string _browsername = "Chrome";
         private static string _environmentname = "dev";
         private static string _baseurl = "https://mechlintech.stage.clocksession.com/app";
-        private static string _basepath = @"C:\Published_Projects\React_App\images\data\";
+        private static string _basepath = @"/app/C:/Published_Projects/React_App/images/data/";
         private static string _driverpath = @"C:\Published_Projects\GhostQA_WebAPI\wwwroot\Driver";
         private static string _ApiUrl = @"https://dev.ghostqa.com/api/AddInBuildTestSuite/SaveInBuiltTestSuites";
         private static bool _isRunHeadless = false;
