d6aa336f83db31ced66b2135fb9079144af319b6
Surendra-M2021 <surendra.mishra@mechlintech.com>
2024-06-06 04:50:40 +0530

    change in live repoting

--- Agent/docker_code/container_run.py
+++ Agent/docker_code/container_run.py
@@ -279,58 +279,26 @@ def get_container_details(container, ref, volume_path):
     container_run = get_container_from_codeengine(ref)
     container_details = get_container(container_run['container_id'])
     
-    while container_details.status != "exited":
+    # logs_path = f"{volume_path}/log.csv"
+    # statistics_path = f"{volume_path}/html-results/statistics.json"
+    # html_path = f"{volume_path}/html-results"
+    
+    while True:
+        logger.info("Liver reporting result")
         # Perform live reporting while container is running
         container_status = container_details.status
         container_labels = container_details.labels
         
         update_container_reporting(container_run['ref'], container_status, container_labels)
         
-        logs_path = f"{volume_path}/log.csv"
-        logger.info(f"logs_path: {logs_path}")
-        statistics_path = f"{volume_path}/html-results/statistics.json"
-        logger.info(f"statistics_path: {statistics_path}")
-        html_path = f"{volume_path}/html-results"
-        logger.info(f"html_path: {html_path}")
-        
-        if os.path.exists(logs_path):
-            try:
-                logger.info("Performing live reporting...")
-                raw_data = csv_to_json(logs_path)
-                raw_data = json.dumps(raw_data)
-                update_container_for_raw_data(container_run['ref'], raw_data)
-                
-                data = get_json_metrics(logs_path)
-                json_data = json.dumps(data)
-                update_container_for_json_data(container_run['ref'], json_data)
-                
-                with open(logs_path, 'rb') as file:
-                    print('monitor_jmx_docker_conatiner: file:', file.read())
-                
-                with open(statistics_path, 'rb') as file:
-                    print('monitor_jmx_docker_conatiner: file:', file.read())
-            except Exception as e:
-                print(f"Error during live reporting: {e}")
-        
-        time.sleep(30)  # Wait for 30 seconds before the next status check
-        container_details = get_container(container_run['container_id'])
-    time.sleep(30) 
-    # Final reporting once the container has exited
-    container_status = container_details.status
-    container_labels = container_details.labels
-    
-    update_container_reporting(container_run['ref'], container_status, container_labels)
-    
-    logs_path = f"{volume_path}/log.csv"
-    logger.info(f"logs_path: {logs_path}")
-    statistics_path = f"{volume_path}/html-results/statistics.json"
-    logger.info(f"statistics_path: {statistics_path}")
-    html_path = f"{volume_path}/html-results"
-    logger.info(f"html_path: {html_path}")
-    
-    if os.path.exists(logs_path):
         try:
-            logger.info("Performing final reporting...")
+            logs_path = f"{volume_path}/log.csv"
+            logger.info(f"logs_path: {logs_path}")
+            statistics_path = f"{volume_path}/html-results/statistics.json"
+            logger.info(f"statistics_path: {statistics_path}")
+            html_path = f"{volume_path}/html-results"
+            logger.info(f"html_path: {html_path}")
+            
             raw_data = csv_to_json(logs_path)
             raw_data = json.dumps(raw_data)
             update_container_for_raw_data(container_run['ref'], raw_data)
@@ -345,10 +313,43 @@ def get_container_details(container, ref, volume_path):
             with open(statistics_path, 'rb') as file:
                 print('monitor_jmx_docker_conatiner: file:', file.read())
         except Exception as e:
-            print(f"Error during final reporting: {e}")
+            print(f"Error during live reporting: {e}")
+        
+        time.sleep(3)  # Wait for 30 seconds before the next status check
+        container_details = get_container(container_run['container_id'])
+
+        # Check if the container has exited
+        if container_details.status == "exited":
+            break
     
-    container_status = container_details.status
-    final_update_container_after_execution(container_run['ref'], container_status)
+    # Final reporting once the container has exited
+    time.sleep(10)
+    try:
+        logger.info("Final reporting result")
+        logs_path = f"{volume_path}/log.csv"
+        logger.info(f"logs_path: {logs_path}")
+        statistics_path = f"{volume_path}/html-results/statistics.json"
+        logger.info(f"statistics_path: {statistics_path}")
+        html_path = f"{volume_path}/html-results"
+        logger.info(f"html_path: {html_path}")
+        
+        raw_data = csv_to_json(logs_path)
+        raw_data = json.dumps(raw_data)
+        update_container_for_raw_data(container_run['ref'], raw_data)
+        
+        data = get_json_metrics(logs_path)
+        json_data = json.dumps(data)
+        update_container_for_json_data(container_run['ref'], json_data)
+        
+        with open(logs_path, 'rb') as file:
+            print('monitor_jmx_docker_conatiner: file:', file.read())
+        
+        with open(statistics_path, 'rb') as file:
+            print('monitor_jmx_docker_conatiner: file:', file.read())
+    except Exception as e:
+        print(f"Error during final reporting: {e}")
+    
+    final_update_container_after_execution(container_run['ref'], container_details.status)
     container.remove()
     print(container.status)
     return True
